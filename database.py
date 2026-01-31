8ad939d2cef173c081e8113f1ec76cfa5ad1e7e0
Kpscreation <108623432+Kpscreation@users.noreply.github.com>
2026-01-31 22:10:57 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:kavit@localhost:5432/RHMS"
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:kavit@localhost:5432/RHMS"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:SwrCmWxTLUmabDsywSRqxfnFONvMUEWD@postgres.railway.internal:5432/railway"
                           
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
